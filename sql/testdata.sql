609adf7f7f10d2c28e2ec157ca339e40486e614b
Moritz Lenz <moritz@faui2k3.org>
2013-09-25 19:07:52 +0200

    add select2 js library

---
+++ sql/testdata.sql
@@ -0,0 +1,11 @@
+INSERT INTO profile (email, name, visibility, natural_languages, programming_languages, perl_stuff)
+        VALUES ('moritz@faui2k3.org', 'Moritz Lenz', 'public',
+            'German=>4,English=>3,French=>1,Norwegian=>1',
+            'C=>1, "C++"=>1, Javascript=>1, SQL=>1, Eiffel=>1,Bash=>1',
+            'Moose=>1,"DBIx::Class"=>1,"Character encodings"=>1'
+        );
+INSERT INTO profile (email, name, visibility, natural_languages, programming_languages)
+        VALUES ('larry@wall.org', 'Larry Wall', 'public',
+            'English=>3,Chinese=>1',
+            'Perl=>1, C=>1'
+        );
