da13e376797f52e25b3ae42a1b7bbb701d9f89ae
Moritz Lenz <moritz@faui2k3.org>
2013-09-25 21:08:24 +0200

    Infrastructure for autocompletion/presets

---
+++ lib/PJ/Model/ResultSet/Profile.pm
@@ -0,0 +1,18 @@
+package PJ::Model::ResultSet::Profile;
+use 5.014;
+use warnings;
+
+use parent 'DBIx::Class::ResultSet';
+
+sub all_entries_for {
+    my ($self, $col) = @_;
+
+    $self->result_source->storage->dbh_do(sub {
+        my ($storage, $dbh) = @_;
+        $dbh->selectcol_arrayref(
+            qq[select skeys($col) AS s, COUNT(*) AS c from profile GROUP BY s ORDER BY c DESC LIMIT 200]
+        );
+    });
+}
+
+1;
