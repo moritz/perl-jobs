e4c5c48a8d64ba2268742dadf407706f052a4924
Moritz Lenz <moritz@faui2k3.org>
2013-10-05 09:24:36 +0200

    [sql] introduce audit log, and table "job"

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,8 @@
+DROP TABLE IF EXISTS job;
 DROP TABLE IF EXISTS login;
 DROP TABLE IF EXISTS skillset;
 DROP TYPE IF EXISTS VISIBILITY_T;
+DROP TYPE IF EXISTS SKILLSET_T;
 
 CREATE OR REPLACE FUNCTION update_modified_column()
 RETURNS TRIGGER AS $$
@@ -26,6 +28,8 @@ CREATE TABLE skillset (
     other_technoligies      HSTORE,
     modified    TIMESTAMP NOT NULL DEFAULT NOW()
 );
+CREATE TRIGGER update_skillset_modtime BEFORE UPDATE ON skillset FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+CREATE TRIGGER audit_skillset AFTER INSERT OR UPDATE OR DELETE ON skillset FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
 
 CREATE TABLE login (
     id          SERIAL PRIMARY KEY,
@@ -33,6 +37,14 @@ CREATE TABLE login (
     skillset    INTEGER REFERENCES skillset (id),
     modified    TIMESTAMP NOT NULL DEFAULT NOW()
 );
-
 CREATE TRIGGER update_login_modtime BEFORE UPDATE ON login FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
-CREATE TRIGGER update_skillset_modtime BEFORE UPDATE ON skillset FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+CREATE TRIGGER audit_login AFTER INSERT OR UPDATE OR DELETE ON login FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+
+CREATE TABLE job (
+    id          SERIAL PRIMARY KEY,
+    skillset    INTEGER REFERENCES skillset (id),
+    entered_by  INTEGER NOT NULL REFERENCES login (id),
+    modified    TIMESTAMP NOT NULL DEFAULT NOW()
+);
+CREATE TRIGGER update_job_modtime BEFORE UPDATE ON login FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+CREATE TRIGGER audit_job AFTER INSERT OR UPDATE OR DELETE ON job FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
